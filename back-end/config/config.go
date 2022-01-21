f7efb6d475bfb2384a915b27797b248b5e5734a3
Saiful Islam Niloy <saifulislam65@iut-dhaka.edu>
2022-01-21 15:38:16 +0600

    student can be added to db

--- back-end/config/config.go
+++ back-end/config/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"context"
-	"fmt"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -11,22 +10,18 @@ import (
 	"os"
 )
 
-func InitDB() {
+func InitDB() *mongo.Client {
 	// Create a new client and connect to the server
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(os.Getenv("DB_URL")))
 	if err != nil {
 		panic(err)
 	}
-	defer func() {
-		if err = client.Disconnect(context.TODO()); err != nil {
-			panic(err)
-		}
-	}()
+
 	// Ping the primary
 	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
 		panic(err)
 	}
-	fmt.Println("Successfully connected and pinged.")
+	return client
 }
 
 func LoadEnvironments() {
@@ -35,4 +30,11 @@ func LoadEnvironments() {
 	if err != nil {
 		log.Fatalf("Error loading .env file")
 	}
+}
+
+func GetStudentsCollection() *mongo.Collection {
+	client := InitDB()
+	// get collection as ref
+	collection := client.Database(os.Getenv("DB_NAME")).Collection(os.Getenv("DB_COLLECTION"))
+	return collection
 }
--- back-end/main.go
+++ back-end/main.go
@@ -1,8 +1,52 @@
 package main
 
-import "main/config"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"main/config"
+	"net/http"
+	"os"
+)
+
+type Student struct {
+	Id         int    `json:"id" bson:"id"`
+	Name       string `json:"name" bson:"name"`
+	University string `json:"university" bson:"university"`
+	Major      string `json:"major" bson:"major"`
+	Email      string `json:"email" bson:"email"`
+}
 
 func main() {
 	config.LoadEnvironments()
-	config.InitDB()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to new server!")
+	})
+
+	http.HandleFunc("/add-student", func(w http.ResponseWriter, r *http.Request) {
+		reqBody, err := ioutil.ReadAll(r.Body)
+
+		student := Student{}
+		json.Unmarshal(reqBody, &student)
+
+		collection := config.GetStudentsCollection()
+		result, err2 := collection.InsertOne(context.TODO(), student)
+
+		fmt.Println(result)
+		fmt.Println(err2)
+
+		fmt.Println(student.Name + " " + student.University)
+
+		if err != nil {
+			log.Fatal(err)
+			fmt.Fprintf(w, "Error")
+		}
+	})
+
+	fmt.Println("APP RUNNING AT-  localhost" + os.Getenv("PORT"))
+	http.ListenAndServe(os.Getenv("PORT"), nil)
+
 }
