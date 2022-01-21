a7e90f0f5c8a5e0a31c77f659ba4e270fb870872
Saiful Islam Niloy <saifulislam65@iut-dhaka.edu>
2022-01-21 22:05:18 +0600

    home page banner

--- front-end/src/components/Banner.js
+++ front-end/src/components/Banner.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import { Button, Card } from 'react-bootstrap';
+import { Card, Container } from 'react-bootstrap';
 
 export default class Banner extends Component {
   render() {
     return (
-      <Card className='banner_background'>
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>Some quick example text to build on the card title and make up the bulk of the cards content.</Card.Text>
-          <Button variant='primary'>Go somewhere</Button>
-        </Card.Body>
-      </Card>
+      <Container fluid={true} className='background'>
+        <br />
+        <Card className='banner_background p-2'>
+          <Card.Body>
+            <Card.Title className='banner_title'>Student Registry System</Card.Title>
+          </Card.Body>
+        </Card>
+      </Container>
     );
   }
 }
--- front-end/src/pages/HomePage.js
+++ front-end/src/pages/HomePage.js
@@ -1,14 +1,11 @@
 import React, { Component, Fragment } from 'react';
-import { Container } from 'react-bootstrap';
 import Banner from '../components/Banner';
 
 class HomePage extends Component {
   render() {
     return (
       <Fragment>
-        <Container fluid={true} className='background'>
-          <Banner />
-        </Container>
+        <Banner />
       </Fragment>
     );
   }
